545e937dae1108345c2262a1d9db133bc7e32ef0
Anna Wiggins <annabunches@gmail.com>
2016-01-21 12:57:35 -0500

    Make config setup more friendly.

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import os
 def get_settings():
   """
   Try to read the settings from file into ConfigParser object.
-  If the config file isn't found, initialize it and bail.
+  If the config file isn't found, initialize it.
   """
   filename = os.path.join(config.app_dir, 'settings.conf')
   settings = ConfigParser()
@@ -13,20 +13,19 @@ def get_settings():
   if os.path.isfile(filename):
     settings.read(filename)
   else:
-    init_settings(settings, filename)
+    _init_settings(settings, filename)
 
   return settings
 
 
-def init_settings(settings, filename):
+def _init_settings(settings, filename):
   settings.add_section('ed_companion')
   settings.add_section('inara')
-  settings.set('ed_companion', 'username', '')
-  settings.set('ed_companion', 'password', '')
-  settings.set('inara', 'username', '')
-  settings.set('inara', 'password', '')
-
+  settings.set('ed_companion', 'username', raw_input("Elite Username (email address): "))
+  settings.set('ed_companion', 'password', raw_input("Elite Password: "))
+  settings.set('inara', 'username', raw_input("Inara Username: "))
+  settings.set('inara', 'password', raw_input("Inara Password: "))
+  print "To change these settings later, edit " + filename
+  
   with open(filename, 'wb') as f:
     settings.write(f)
-
-  raise Exception("Missing configuration. Please edit %s and run the program again." % filename)
